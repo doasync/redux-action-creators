d54b865f83f856f898b2acf77784484de196cf96
doasync <doasync@gmail.com>
2017-12-12 22:37:42 +0300

    Fixes: default params, meta, error messages

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ export const $$ = TYPE;
 
 export const actionCreator = (type, fn) => {
   if (typeof type !== 'string' || !type) {
-    throw new Error('Action type is required [string]');
+    throw new Error('Action type should be a non-empty string');
   }
   const $actionCreator = (payload, meta) => {
-    const action = { type, payload, meta };
-    const result = fn ? fn(action) : undefined;
+    const action = meta !== undefined ? { type, payload, meta } : { type, payload };
+    const result = fn !== undefined ? fn(action) : undefined;
     return result !== undefined ? result : action;
   };
   return Object.defineProperty($actionCreator, TYPE, { value: type });
 };
 
 export const actionCreatorFactory = (params = {}) => (type, fn) => {
-  const { actionsCreator: subActionsCreator, subTypes, prefix } = params;
+  const {
+    actionsCreator: subActionsCreator = actionCreator,
+    subTypes,
+    prefix = ''
+  } = params;
   const $actionCreator = actionCreator(`${prefix}${type}`, fn);
   const subs = Object.entries(subTypes).reduce((result, [symbolName, symbol]) => {
     if (typeof symbol === 'symbol') {
       result[symbol] = subActionsCreator(`${prefix}${type}[${symbolName}]`);
     } else {
-      throw new Error('You should use symbols for subTypes');
+      throw new Error(`Sub-type ${symbolName} should be a symbol`);
     }
     return result;
   }, {});
